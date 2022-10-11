5bd0c396a992217343aa9a2272f04d03e30a89f7
OktayCavus <oktaycvs61@gmail.com>
2022-10-11 11:51:14 +0300

    loading screen

--- lib/view/loading_screen.dart
+++ lib/view/loading_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter/material.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
 
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
